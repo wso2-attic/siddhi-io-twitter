25bc78a8d9ab0eb93c19eeeed301363dee479e96
suganya <suganya@wso2.com>
2018-02-23 10:54:17 +0530

    Add "query" is a mandatory parameter for polling mode.

--- component/src/main/java/org/wso2/extension/siddhi/io/twitter/source/TwitterConsumer.java
+++ component/src/main/java/org/wso2/extension/siddhi/io/twitter/source/TwitterConsumer.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.extension.siddhi.io.twitter.source;
 
 import org.apache.log4j.Logger;
@@ -194,6 +212,7 @@ public class TwitterConsumer {
 
     /**
      * Validates the parameters that allows specific values.
+     *
      * @param mode
      * @param filterLevel
      * @param resultType
@@ -222,11 +241,13 @@ public class TwitterConsumer {
                 }
             } else {
                 if (query.isEmpty()) {
-                    if (log.isDebugEnabled()) {
-                        log.debug("In polling mode, you can only give these following parameters. " +
-                                "If you give any other parameters, they will be ignored.\n" +
-                                "{query, geocode, max.id, since.id, language, result.type, until}");
-                    }
+                    throw new SiddhiAppCreationException("In polling mode, query is a mandatory parameter.");
+                }
+
+                if (log.isDebugEnabled()) {
+                    log.debug("In polling mode, you can only give these following parameters. " +
+                            "If you give any other parameters, they will be ignored.\n" +
+                            "{query, geocode, max.id, since.id, language, result.type, until}");
                 }
             }
         } else {
@@ -236,13 +257,11 @@ public class TwitterConsumer {
         if (!filterLevels.contains(filterLevel)) {
             throw new SiddhiAppCreationException("There are only three possible values for filterlevel :" +
                     " low or medium or none");
-
         }
 
         if (!resultTypes.contains(resultType)) {
             throw new SiddhiAppCreationException("There are only three possible values for result.type :" +
                     " mixed or popular or recent");
-
         }
     }
 }
